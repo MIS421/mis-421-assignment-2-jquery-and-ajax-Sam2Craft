8c5c05f80f24ce04c6764bde11786756eb786af2
Sam Mabry <srmabry@crimson.ua.edu>
2023-02-15 08:55:17 -0600

    Final FINAL

--- SearchEngineHW/SearchEngineHW/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
+++ SearchEngineHW/SearchEngineHW/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -1,5 +1,9 @@
 ﻿var len;
 var results = '';
+var toggle = false;
+
+$("button").button();
+
 
 function apiSearch() {
     var params = {
@@ -10,22 +14,42 @@ function apiSearch() {
     };
 
     $.ajax({
-        url: 'my-api-url' + $.param(params),
+        url: "https://api.bing.microsoft.com/v7.0/search?" + $.param(params),
         beforeSend: function (xhrObj) {
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "af9c6ad42a3f4932b8b8510cd473f74b");
         },
         type: "GET",
     })
         .done(function (data) {
             len = data.webPages.value.length;
             for (i = 0; i < len; i++) {
+
                 results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
             }
-
+            debugger;
             $('#searchResults').html(results);
-            $('#searchResults').dialog();
+            $('#searchResults').dialog({
+                minWidth: 800
+            });
+            results = '';
         })
         .fail(function () {
             alert("error");
         });
+}
+
+function toggleBackground() {
+    $("body").toggleClass("backgroundOne backgroundTwo")
+}
+
+function showTime() {
+    var d = new Date();
+    var n = d.toLocaleTimeString([], { timeStyle: 'short' });
+    console.log(n);
+    $("#time").toggleClass("hidden visible");
+    $("#time").text(n);
+    $("#time").dialog();
+}
+
+function renderSearch() {
 }
--- SearchEngineHW/SearchEngineHW/wwwroot/js/site.js
+++ SearchEngineHW/SearchEngineHW/wwwroot/js/site.js
@@ -4,14 +4,16 @@ var toggle = false;
 
 $("button").button();
 
-
 function apiSearch() {
-    var params = {
-        "q": $("#query").val(),
-        "count": "50",
-        "offset": "0",
-        "mkt": "en-us"
-    };
+
+        var params = {
+            "q": $("#query").val(),
+            "count": "50",
+            "offset": "0",
+            "mkt": "en-us"
+            }
+    
+
 
     $.ajax({
         url: "https://api.bing.microsoft.com/v7.0/search?" + $.param(params),
@@ -23,10 +25,8 @@ function apiSearch() {
         .done(function (data) {
             len = data.webPages.value.length;
             for (i = 0; i < len; i++) {
-
                 results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
             }
-            debugger;
             $('#searchResults').html(results);
             $('#searchResults').dialog({
                 minWidth: 800
@@ -51,5 +51,34 @@ function showTime() {
     $("#time").dialog();
 }
 
-function renderSearch() {
+function feelingLucky() {
+    var params = {
+        "q": '',
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    }
+
+    $.ajax({
+        url: "https://api.bing.microsoft.com/v7.0/search?" + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "af9c6ad42a3f4932b8b8510cd473f74b");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            for (i = 0; i < len; i++) {
+                results = "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+            }
+            $('#searchResults').html(results);
+            $('#searchResults').dialog({
+                minWidth: 800
+            });
+            results = '';
+        })
+        .fail(function () {
+            alert("error");
+        });
+
 }
